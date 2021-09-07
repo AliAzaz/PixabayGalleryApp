faa100264953b2d670b42ccd95a74fa0c245b881
AliAzaz <ali.azaz.alam@hotmail.com>
2021-09-07 14:16:22 +0500

    *commit

--- app/src/main/java/com/example/pixabaygalleryapp/utils/CONSTANTS.kt
+++
@@ -1,9 +0,0 @@
-package com.example.pixabaygalleryapp.utils
-
-object CONSTANTS {
-    const val BASE_URL = "https://pixabay.com/"
-    const val API_KEY = "key"
-    const val PAGE = "page"
-    const val CATEGORY = "category"
-    const val SEARCH = "q"
-}
