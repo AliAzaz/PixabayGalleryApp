8de7927521dc28886a1cf7eea081f036a22c7b1d
AliAzazAlam <ali.azaz.alam@hotmail.com>
2023-07-25 17:23:55 +0500

    I have worked on: - Commit

--- app/src/main/java/com/example/pixabaygalleryapp/base/BaseUseCase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.pixabaygalleryapp.base
-
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.flow.Flow
-
-interface BaseUseCase<in P, out R> {
-    operator fun invoke(params: P? = null): Flow<R>
-    fun getDispatcher(): CoroutineDispatcher
-}
